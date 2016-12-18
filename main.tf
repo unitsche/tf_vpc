b9c24dba63c8793536fdc3cca8a29a1c3e679c12
un <un@ulis-MacBook.fritz.box>
2016-12-18 12:20:41 +0100

    reworked according to TFB

--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
-# vers 1
-
 resource "aws_vpc" "environment" {
   cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
-
   tags {
     Name = "${var.environment}"
   }
@@ -12,7 +9,6 @@ resource "aws_vpc" "environment" {
 
 resource "aws_internet_gateway" "environment" {
   vpc_id = "${aws_vpc.environment.id}"
-
   tags {
     Name = "${var.environment}-igw"
   }
@@ -27,9 +23,8 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
-  count  = "${length(var.private_subnets)}"
+  count = "${length(var.private_subnets)}"
   vpc_id = "${aws_vpc.environment.id}"
-
   tags {
     Name = "${var.environment}-private"
   }
@@ -42,21 +37,19 @@ resource "aws_route" "public_internet_gateway" {
 }
 
 resource "aws_route" "private_nat_gateway" {
-  count                  = "${length(var.private_subnets)}"
-  route_table_id         = "${aws_route_table.private.*.id[count.index]}"
+  count = "${length(var.private_subnets)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.environment.*.id[count.index]}"
+  nat_gateway_id = "${element(aws_nat_gateway.environment.*.id, count.index)}"
 }
 
 resource "aws_subnet" "public" {
   vpc_id                  = "${aws_vpc.environment.id}"
   cidr_block              = "${var.public_subnets[count.index]}"
   map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
-
   tags {
     Name = "${var.environment}-public-${count.index}"
   }
-
   count = "${length(var.public_subnets)}"
 }
 
@@ -64,23 +57,21 @@ resource "aws_subnet" "private" {
   vpc_id                  = "${aws_vpc.environment.id}"
   cidr_block              = "${var.private_subnets[count.index]}"
   map_public_ip_on_launch = "false"
-
   tags {
     Name = "${var.environment}-private-${count.index}"
   }
-
   count = "${length(var.private_subnets)}"
 }
 
 resource "aws_route_table_association" "private" {
   count          = "${length(var.private_subnets)}"
-  subnet_id      = "${aws_subnet.private.*.id[count.index]}"
-  route_table_id = "${aws_route_table.private.*.id[count.index]}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
 }
 
 resource "aws_route_table_association" "public" {
   count          = "${length(var.public_subnets)}"
-  subnet_id      = "${aws_subnet.public.*.id[count.index]}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
@@ -93,13 +84,13 @@ resource "aws_eip" "environment" {
 resource "aws_nat_gateway" "environment" {
   count = "${length(var.public_subnets)}"
 
-  allocation_id = "${aws_eip.environment.*.id[count.index]}"
-  subnet_id     = "${aws_subnet.public.*.id[count.index]}"
+  allocation_id = "${element(aws_eip.environment.*.id, count.index)}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
 }
 
 resource "aws_security_group" "bastion" {
   vpc_id      = "${aws_vpc.environment.id}"
-  name        = "${var.environment}-bastion-host"
+  name        = "${var.environment}-bastion_host"
   description = "Allow SSH to bastion host"
 
   ingress {
@@ -122,9 +113,8 @@ resource "aws_security_group" "bastion" {
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags {
-    Name = "${var.environment}-bastion-sg"
+    Name        = "${var.environment}-bastion-sg"
   }
 }
 
@@ -134,10 +124,10 @@ resource "aws_instance" "bastion" {
   key_name                    = "${var.key_name}"
   monitoring                  = true
   vpc_security_group_ids      = ["${aws_security_group.bastion.id}"]
-  subnet_id                   = "${aws_subnet.public.*.id[0]}"
+  subnet_id                   = "${element(aws_subnet.public.*.id, 0)}"
   associate_public_ip_address = true
 
   tags {
-    Name = "${var.environment}-bastion"
+    Name        = "${var.environment}-bastion"
   }
 }
